b467d73447a32b2f5cbea81cd1b0275e7964e7b1
Herbert Attenberger <herbsl@a-land.de>
2014-04-27 14:45:36 +0200

    moved ioc-loader from local.php to global.php

--- app/start/global.php
+++ app/start/global.php
@@ -79,3 +79,6 @@ App::down(function()
 */
 
 require app_path().'/filters.php';
+
+// manually added
+require app_path().'/ioc.php";
--- app/start/local.php
+++ app/start/local.php
@@ -1,4 +1,3 @@
 <?php
 
-require(app_path() . '/lib/Asset.php');
 //
