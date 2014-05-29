96bb21b1259dba77077625377da4291047e56090
Herbert Attenberger <herbsl@a-land.de>
2014-05-29 11:57:56 +0200

    new build

--- app/views/bike/empty.blade.php
+++ app/views/bike/empty.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="container hb-container hb-font-size-md">
 	@include('bike.index-header')
-	<p>Leider haben wir momentan {{{ $text }}} keinen passenden Artikel in unserem Online Sortiment. In unserem Ladengesch&auml;ft bieten wir Ihnen eine erweiterte Auswahl.</p><p>Wir freuen uns, von Ihnen zu h&ouml;ren.</p>
+	<p>Leider haben wir momentan {{{ $text }}} keinen passenden Artikel in unserem Online Sortiment.</p></p>In unserem Ladengesch&auml;ft bieten wir Ihnen eine erweiterte Auswahl. Wir freuen uns, von Ihnen zu h&ouml;ren.</p>
 </div>
 @stop
