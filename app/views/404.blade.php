09355db25b52e25c5d74023eddef5bb54be83b4b
Herbert Attenberger <herbsl@a-land.de>
2014-05-29 19:08:06 +0200

    new build

--- app/views/404.blade.php
+++ app/views/404.blade.php
@@ -5,6 +5,7 @@
 	<div class="page-header">	
 		<h1>Hoppala</h1>
 	</div>
-	<p>Die aufgerufene Adresse gibt es auf unserer Website nicht. Verwenden Sie doch einfach unsere Navigationsleisten.</p>
+	<p>Die aufgerufene Adresse gibt es auf unserer Website nicht. Verwenden Sie doch einfach das Men&uuml; zum weiteren surfen.</p>
+	<p>Viel Spa&szlig; auf www.huberbikes.de!</p>
 </div>
 @stop
