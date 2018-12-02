81eaa226e40f4e32fc5da9143709198d345b78f8
Herbert Attenberger <herbsl@a-land.de>
2018-12-02 09:32:19 +0100

    Winteroeffnungszeiten

--- app/views/oeffnungszeiten-content.blade.php
+++ app/views/oeffnungszeiten-content.blade.php
@@ -1,14 +1,15 @@
 <!--p class="bg-info" style="padding: 15px">Wir befinden uns vom 10.09. bis einschlie&szlig;lich 22.09. im Urlaub.<p-->
+<p class="bg-info" style="padding: 15px">Ab Dezember ge&auml;nderte &Ouml;ffnungszeiten:<p>
 <div class="table-responsive">
 	<table class="table table-striped">
 		<tbody>
 		 	<tr>
 				<td>Mo</td>
-				<td>14.30-18.00h</td>
+				<td>geschlossen</td>
 			</tr>
 			<tr>
 				<td>Di</td>
-				<td>14.30-18.00h</td>
+				<td>geschlossen</td>
 			</tr>
 			<tr>
 				<td>Mi</td>
@@ -24,7 +25,7 @@
 			</tr>
 			<tr>
 				<td>Sa</td>
-				<td>09.00-13.00h</td>
+				<td>08.30-13.00h</td>
 			</tr>
 		</tbody>
 	</table>
