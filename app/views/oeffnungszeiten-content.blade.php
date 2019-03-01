5805c486ecb771cb99285a326d3db17c929f1473
Herbert Attenberger <herbsl@a-land.de>
2019-03-01 11:38:01 +0100

    Winteroeffungszeiten deaktiviert

--- app/views/oeffnungszeiten-content.blade.php
+++ app/views/oeffnungszeiten-content.blade.php
@@ -1,15 +1,15 @@
 <!--p class="bg-info" style="padding: 15px">Wir befinden uns vom DD.MM. bis einschlie&szlig;lich DD.MM. im Urlaub.<p-->
-<p class="bg-info" style="padding: 15px">Ab Dezember gelten unsere Winter&ouml;ffnungszeiten.<p>
+<!--p class="bg-info" style="padding: 15px">Ab Dezember gelten unsere Winter&ouml;ffnungszeiten.<p-->
 <div class="table-responsive">
 	<table class="table table-striped">
 		<tbody>
 		 	<tr>
 				<td>Mo</td>
-				<td>geschlossen</td>
+				<td>14.30-18.00h</td>
 			</tr>
 			<tr>
 				<td>Di</td>
-				<td>geschlossen</td>
+				<td>14.30-18.00h</td>
 			</tr>
 			<tr>
 				<td>Mi</td>
